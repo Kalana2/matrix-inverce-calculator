652f245cdc9c7c37b804efe1a952a243c90b0c31
Kalana <Kalana@email>
2024-10-23 21:12:55 +0530

    cofactor

--- matrix_invrese.py
+++ matrix_invrese.py
@@ -18,3 +18,33 @@ def determinant (a):
         totalDet += sign * a[0][col] * determinant(minor)
     
     return totalDet
+
+
+
+def cofactor (a):
+    if(len(a)  == 2):
+        return a[0][0] * a[1][1] - a[0][1] * a[1][0]
+      
+
+    cofactorMatrix = []
+
+    for row in range(len(a)):
+        cofactorRow = []
+        for col in range(len(a)):
+                
+            minor = [r[:col] + r[col+1:] for r in (a[:row] + a[row + 1:])]
+
+            minorDet = determinant(minor)
+
+            sign = (-1)**(row + col)
+                
+            cofactorRow.append(sign * minorDet)
+    
+        cofactorMatrix.append(cofactorRow)
+
+    return cofactorMatrix
+
+def inverse(a):
+
+    #calculate ajoint matrix
+    
